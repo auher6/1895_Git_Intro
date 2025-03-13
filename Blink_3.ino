8f882aaec2b1cedc1c690ee895037edaa39271b0
micahsmithh <bluemicah8@gmail.com>
2025-03-13 12:36:55 -0400

    my name

--- Blink_3.ino
+++ Blink_3.ino
@@ -22,6 +22,7 @@
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
 */
 
+// Micah Smith
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
